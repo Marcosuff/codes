36bac6973e4aa05e05d930ac0ed54ff641c2f01c
Marcos Santana <marcosvs_santana@hotmail.com>
2017-08-18 15:01:48 -0300

    Add files via upload

--- substructure_search.py
+++ substructure_search.py
@@ -1,46 +1,44 @@
 # Import modules
-from subprocess import call, check_output
-import re
+from subprocess import check_output
 import sys
-import pandas as pd
 import os
+import pandas as pd
 
-with open('fragment_library.smi') as x: # Query SMILE file (format: smiles compd_name)
-	smiles = [line for line in x] 
+#### This script uses OpenBabel for substructure search
+
+with open('pures.smi') as x: # Query SMILE file (format: smiles compd_name)
+	smiles = [line for line in x]
+dt = [] # Create a dict to store OpenBabel results
 
 def similarity_search(smi):
 	""" This function calls openbabel similarity search on the list of SMILES submitted by the user"""
 
 	fs_file = 'drugbank_approved.fs' # FP2 fingerprints file
-	#max_similarity = '10' # max number of similar compounds to find
-	
-	
-	output = 'known_drugs_substructure.smi' # Output file for FDA approved drugs
-	#pubchem_out = '.similar_pubchem.out' # Output file for PubChem similarity analysis
 
-	with open(output, 'w') as y:
-		for mol in smi:
+	for mol in smi:
 
-			#### Similarity Search using DrugBank All/Approved Drugs (LOCALLY) ####
+		smiles,mol_name = str(mol.split()[0]),str(mol.split()[1])
 
-			output_message = str(mol)
-			#babel mymols.fs -ifs -sN#Cc1ccccc1C#N results.smi
-			a = check_output('(obabel %s -ifs -s "%s" -O %s -xt)' % (fs_file, mol, output), shell=True)
-			y.write(('%s%s') % (output_message, a)) # Output format: query_SMILES;similar_compounds;tanimoto_score
+		a = (check_output('(obabel %s -ifs -s "%s" -otxt -xt)' % (fs_file, mol), shell=True))
+		supers = a.split() # Split a at '\n' to get the superstructures separated by a comma
 
+		if len(supers) > 0: # Append only fragments match a drug
+			dt.append({'Fragment': mol_name, 'Smiles' : smiles, 'Superstructures': (", ".join(supers))})
 
 	print ('Similarity against FDA dataset complete. Our gremlins are working on the output file. Please wait...')
-		
-			#### Similarity Search on PubChem server (NOT IMPLEMENTED YET) ####
-
-	#with open(pubchem_out, 'w') as g:
-		#for mol in smi:
-
-			#output_message = 'Calculating similars to ' + str(mol) + '\n'
-			#a = get_compounds(mol, 'smiles', searchtype='similarity', listkey_count=10, Threshold=80)
-			#g.write(('%s%s%s') % (output_message, a, '\n')) # Output format: query_SMILES;similar_compounds;tanimoto_score
-
-	#print ('PubChem analysis complete. The results were store in %s') % (pubchem_out)
 
 # Call function
 similarity_search(smiles)
+
+# Create dataframe
+df = pd.DataFrame(dt)
+
+new_df = (df['Superstructures'].str.split(',', expand=True)
+.stack().reset_index(level=0).set_index('level_0')
+.rename(columns={0:'Superstructures'})
+.join(df.drop('Superstructures',1), how='left')
+.sort_index(axis=1)
+) # Split the values on Superstructures columms to different rows
+
+# Save to csv file
+new_df.to_csv('substructures.csv')
